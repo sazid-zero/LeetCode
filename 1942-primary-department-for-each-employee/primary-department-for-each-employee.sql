2e58d927fd5e6c81e18ddbac769e381dbea077d8
Sharif Mahmud Sazid <107400335+sazid-zero@users.noreply.github.com>
2026-02-05 21:24:25 +0600

    Time: 479 ms (91.98%) | Memory: 0B (100.00%) - LeetSync

--- 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -1,8 +1,18 @@
 # Write your MySQL query statement below
-SELECT employee_id, department_id
+/*SELECT employee_id, department_id
 FROM (
     SELECT *,
            COUNT(*) OVER (PARTITION BY employee_id) AS cnt
     FROM Employee
 ) e
-WHERE (cnt = 1) OR (primary_flag = 'Y');
+WHERE (cnt = 1) OR (primary_flag = 'Y'); */
+SELECT employee_id, department_id
+FROM Employee
+WHERE primary_flag = 'Y'
+
+UNION
+
+SELECT employee_id, department_id
+FROM Employee
+GROUP BY employee_id
+HAVING COUNT(*) = 1;
