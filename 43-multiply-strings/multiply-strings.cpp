ec042af8cf4a00c10f2fd56eda26e64ae008360e
Sharif Mahmud Sazid <107400335+sazid-zero@users.noreply.github.com>
2026-04-21 21:18:17 +0600

    Time: 3 ms (61.00%) | Memory: 9.7 MB (64.47%) - LeetSync

---
+++ 43-multiply-strings/multiply-strings.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string multiply(string num1, string num2) {
+        if (num1 == "0" || num2 == "0") return "0";
+
+        int n1 = num1.size();
+        int n2 = num2.size();
+        vector<int> res(n1 + n2, 0);
+        for (int i = n1 - 1; i >= 0; --i) {
+            int d1 = num1[i] - '0';
+            for (int j = n2 - 1; j >= 0; --j) {
+                int d2 = num2[j] - '0';
+                int sum = res[i + j + 1] + d1 * d2;
+                res[i + j + 1] = sum % 10;
+                res[i + j] += sum / 10;
+            }
+        }
+        string ans;
+        int i = 0;
+        while (i < (int)res.size() && res[i] == 0) ++i;
+        for (; i < (int)res.size(); ++i) {
+            ans.push_back(char('0' + res[i]));
+        }
+        return ans.empty() ? "0" : ans;
+    }
+};
