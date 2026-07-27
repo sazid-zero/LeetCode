901973581d171b518cff62e30b42306f38d276fb
Sharif Mahmud Sazid <107400335+sazid-zero@users.noreply.github.com>
2026-07-27 23:33:28 +0600

    Time: 0 ms (100.00%) | Memory: 14 MB (88.87%) - LeetSync

--- 89-gray-code/gray-code.cpp
+++ 89-gray-code/gray-code.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
     vector<int> grayCode(int n) {
-        vector<int> res;
-        int size = 1 << n;            
-
-        res.reserve(size);
-        for (int i = 0; i < size; ++i) {
-            res.push_back(i ^ (i >> 1)); 
+        vector<int> ans;
+        ans.reserve(1 << n);
+        for (int i = 0; i < (1 << n); ++i) {
+            ans.push_back(i ^ (i >> 1));
         }
-        return res;
+        return ans;
     }
 };
