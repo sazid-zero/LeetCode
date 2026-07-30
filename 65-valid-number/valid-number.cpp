ea2bca07f7a8a71e07a9fe8017c2476b6c99d1ef
Sharif Mahmud Sazid <107400335+sazid-zero@users.noreply.github.com>
2026-07-30 22:51:07 +0600

    Time: 0 ms (100.00%) | Memory: 8 MB (87.24%) - LeetSync

---
+++ 65-valid-number/valid-number.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isNumber(string s) {
+        bool num = false, exp = false, dot = false;
+
+        for (int i = 0; i < (int)s.size(); i++) {
+            char c = s[i];
+
+            if (c >= '0' && c <= '9') {
+                num = true;
+            } else if (c == '+' || c == '-') {
+                if (i > 0 && s[i - 1] != 'e' && s[i - 1] != 'E')
+                    return false;
+            } else if (c == '.') {
+                if (dot || exp)
+                    return false;
+                dot = true;
+            } else if (c == 'e' || c == 'E') {
+                if (exp || !num)
+                    return false;
+                exp = true;
+                num = false;
+            } else {
+                return false;
+            }
+        }
+
+        return num;
+    }
+};
