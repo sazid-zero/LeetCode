9ce22c08af874c383f958fe8322ff44565b3911d
Sharif Mahmud Sazid <107400335+sazid-zero@users.noreply.github.com>
2026-07-31 23:19:33 +0600

    Time: 7 ms (60.86%) | Memory: 13.3 MB (43.07%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,25 +1,19 @@
 class Solution {
 public:
     int minDistance(string word1, string word2) {
-        int n = word1.size();
-        int m = word2.size();
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
-        for (int i = 0; i <= n; ++i) dp[i][0] = i;
-       
-        for (int j = 0; j <= m; ++j) dp[0][j] = j;
-
-        for (int i = 1; i <= n; ++i) {
-            for (int j = 1; j <= m; ++j) {
+        int m = word1.size(), n = word2.size();
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1));
+        for (int i = 0; i <= m; ++i) dp[i][0] = i;
+        for (int j = 0; j <= n; ++j) dp[0][j] = j;
+        for (int i = 1; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
                 if (word1[i - 1] == word2[j - 1]) {
-                    dp[i][j] = dp[i - 1][j - 1]; 
+                    dp[i][j] = dp[i - 1][j - 1];
                 } else {
-                    int insertOp = dp[i][j - 1] + 1;    
-                    int deleteOp = dp[i - 1][j] + 1;    
-                    int replaceOp = dp[i - 1][j - 1] + 1; 
-                    dp[i][j] = min({insertOp, deleteOp, replaceOp});
+                    dp[i][j] = 1 + min({dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]});
                 }
             }
         }
-        return dp[n][m];
+        return dp[m][n];
     }
 };
