c4882edde1d08f686b29322c9606578f5b9899b1
Sharif Mahmud Sazid <107400335+sazid-zero@users.noreply.github.com>
2026-08-06 23:46:01 +0600

    Time: 8 ms (27.57%) | Memory: 15.4 MB (8.58%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
@@ -1,48 +1,23 @@
-/*
-// Definition for a Node.
-class Node {
-public:
-    int val;
-    Node* next;
-    Node* random;
-    
-    Node(int _val) {
-        val = _val;
-        next = nullptr;
-        random = nullptr;
-    }
-};
-*/
-
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
         if (!head) return nullptr;
-        
+
+        unordered_map<Node*, Node*> mp;
         Node* cur = head;
+
         while (cur) {
-            Node* copy = new Node(cur->val);
-            copy->next = cur->next;
-            cur->next = copy;
-            cur = copy->next;
-        }
-        
-        cur = head;
-        while (cur) {
-            if (cur->random) cur->next->random = cur->random->next;
-            cur = cur->next->next;
-        }
-        
-        Node* dummy = new Node(0);
-        Node* copyCur = dummy;
-        cur = head;
-        while (cur) {
-            copyCur->next = cur->next;
-            cur->next = cur->next->next;
+            mp[cur] = new Node(cur->val);
             cur = cur->next;
-            copyCur = copyCur->next;
         }
-        
-        return dummy->next;
+
+        cur = head;
+        while (cur) {
+            if (cur->next) mp[cur]->next = mp[cur->next];
+            if (cur->random) mp[cur]->random = mp[cur->random];
+            cur = cur->next;
+        }
+
+        return mp[head];
     }
 };
