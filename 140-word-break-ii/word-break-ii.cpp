a19b1fca556ba5bedae43601bf9488d6ef236246
Sharif Mahmud Sazid <107400335+sazid-zero@users.noreply.github.com>
2026-08-07 00:19:36 +0600

    Time: 0 ms (100.00%) | Memory: 10.2 MB (79.08%) - LeetSync

--- 140-word-break-ii/word-break-ii.cpp
+++ 140-word-break-ii/word-break-ii.cpp
@@ -2,27 +2,49 @@ class Solution {
 public:
     vector<string> wordBreak(string s, vector<string>& wordDict) {
         unordered_set<string> dict(wordDict.begin(), wordDict.end());
-        unordered_map<int, vector<string>> memo;
-        return dfs(0, s, dict, memo);
-    }
+        int n = s.size();
+        vector<bool> dp(n + 1, false);
+        dp[0] = true;
 
-    vector<string> dfs(int start, string& s, unordered_set<string>& dict,
-                       unordered_map<int, vector<string>>& memo) {
-        if (memo.count(start)) return memo[start];
-        vector<string> res;
-        if (start == s.size()) {
-            res.push_back("");
-            return memo[start] = res;
-        }
-        for (int end = start + 1; end <= s.size(); ++end) {
-            string word = s.substr(start, end - start);
-            if (!dict.count(word)) continue;
-            vector<string> sub = dfs(end, s, dict, memo);
-            for (string& tail : sub) {
-                if (tail.empty()) res.push_back(word);
-                else res.push_back(word + " " + tail);
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (dp[j] && dict.count(s.substr(j, i - j))) {
+                    dp[i] = true;
+                    break;
+                }
             }
         }
-        return memo[start] = res;
+
+        vector<string> res;
+        if (!dp[n]) return res;
+
+        vector<string> path;
+        backtrack(s, 0, dict, dp, path, res);
+        return res;
+    }
+
+private:
+    void backtrack(const string &s, int start, const unordered_set<string> &dict,
+                   const vector<bool> &dp, vector<string> &path, vector<string> &res) {
+        int n = s.size();
+        if (start == n) {
+            string sentence;
+            for (int i = 0; i < (int)path.size(); ++i) {
+                if (i) sentence += ' ';
+                sentence += path[i];
+            }
+            res.push_back(sentence);
+            return;
+        }
+
+        for (int end = start + 1; end <= n; ++end) {
+            if (!dp[end]) continue;
+            string word = s.substr(start, end - start);
+            if (!dict.count(word)) continue;
+
+            path.push_back(word);
+            backtrack(s, end, dict, dp, path, res);
+            path.pop_back();
+        }
     }
 };
