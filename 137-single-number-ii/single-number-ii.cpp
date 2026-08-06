42952557facc3fe408331ff741e6f82d88a387b6
Sharif Mahmud Sazid <107400335+sazid-zero@users.noreply.github.com>
2026-08-06 23:40:39 +0600

    Time: 0 ms (100.00%) | Memory: 13.3 MB (87.71%) - LeetSync

--- 137-single-number-ii/single-number-ii.cpp
+++ 137-single-number-ii/single-number-ii.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int ans = 0;
-        for (int bit = 0; bit < 32; ++bit) {
-            int cnt = 0;
-            for (int x : nums) {
-                if ((x >> bit) & 1) cnt++;
-            }
-            if (cnt % 3) ans |= (1 << bit);
+        int ones = 0, twos = 0;
+        for (int x : nums) {
+            ones = (ones ^ x) & ~twos;
+            twos = (twos ^ x) & ~ones;
         }
-        return ans;
+        return ones;
     }
 };
