84587ca164bd488a3c9a4b617ac2f1370b1a37c5
Sharif Mahmud Sazid <107400335+sazid-zero@users.noreply.github.com>
2026-08-09 22:18:00 +0600

    Time: 0 ms (100.00%) | Memory: 22.8 MB (78.31%) - LeetSync

--- 143-reorder-list/reorder-list.cpp
+++ 143-reorder-list/reorder-list.cpp
@@ -1,45 +1,40 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-
 class Solution {
 public:
     void reorderList(ListNode* head) {
         if (!head || !head->next) return;
-        
+
         ListNode* slow = head;
         ListNode* fast = head;
+
         while (fast->next && fast->next->next) {
             slow = slow->next;
             fast = fast->next->next;
         }
-        
-        ListNode* prev = nullptr;
-        ListNode* cur = slow->next;
+
+        ListNode* second = slow->next;
         slow->next = nullptr;
-        while (cur) {
-            ListNode* nxt = cur->next;
-            cur->next = prev;
-            prev = cur;
-            cur = nxt;
-        }
-        
-        ListNode* first = head;
-        ListNode* second = prev;
+
+        ListNode* prev = nullptr;
+
         while (second) {
-            ListNode* tmp1 = first->next;
-            ListNode* tmp2 = second->next;
+            ListNode* next = second->next;
+            second->next = prev;
+            prev = second;
+            second = next;
+        }
+
+        second = prev;
+        ListNode* first = head;
+
+        while (second) {
+            ListNode* firstNext = first->next;
+            ListNode* secondNext = second->next;
+
             first->next = second;
-            second->next = tmp1;
-            first = tmp1;
-            second = tmp2;
+            second->next = firstNext;
+
+            first = firstNext;
+            second = secondNext;
         }
     }
 };
