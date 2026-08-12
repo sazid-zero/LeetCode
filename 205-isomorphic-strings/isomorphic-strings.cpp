d5a8571e7155d06272aaa359ae7f3521e988f1e6
Sharif Mahmud Sazid <107400335+sazid-zero@users.noreply.github.com>
2026-08-12 19:07:49 +0600

    Time: 0 ms (100.00%) | Memory: 9.4 MB (58.80%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -2,20 +2,15 @@ class Solution {
 public:
     bool isIsomorphic(string s, string t) {
         if (s.size() != t.size()) return false;
-        vector<int> m1(256, -1), m2(256, -1);
 
-        for (int i = 0; i < (int)s.size(); ++i) {
-            unsigned char c1 = s[i];
-            unsigned char c2 = t[i];
+        vector<int> mapS(256, -1);
+        vector<int> mapT(256, -1);
 
-            if (m1[c1] == -1 && m2[c2] == -1) {
-                m1[c1] = c2;
-                m2[c2] = c1;
-            } else {
-                if (m1[c1] != c2 || m2[c2] != c1) return false;
-            }
+        for (int i = 0; i < s.size(); i++) {
+            if (mapS[s[i]] != mapT[t[i]]) return false;
+            mapS[s[i]] = i;
+            mapT[t[i]] = i;
         }
-
         return true;
     }
 };
